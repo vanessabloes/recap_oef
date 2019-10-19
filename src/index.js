11468483ba6b27f2ffbb366d0ffcbe01e6379ae8
vanessabloes <vanessa.bloes@student.howest.be>
2019-10-19 19:04:13 +0200

    inladen van coffees en aanmaak en invullen van HTML elementen op basis van coffees.json bestand

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
 import './style.css';
 import {devineLog} from './js/utility/helpers';
+import data from './assets/data/coffees.json';
 
 devineLog('Hey, ik ben een JS file');
 const arr = [1, 2, 3];
 
 const ES6Stuff = () => devineLog('Ik kan ES6 stuff aan', ...arr);
 ES6Stuff();
+
+
+//HTML-structuur aanmaken
+
+const makeHTML = ($pricelist, coffeeName, coffeePrice) => {
+  const $list = document.createElement(`li`);
+  $list.classList.add(`price`);
+  $pricelist.appendChild($list);
+
+  $pricelist.appendChild($list);
+  const $priceButton = document.createElement(`a`);
+  $list.appendChild($priceButton);
+  $priceButton.classList.add(`price__button`);
+
+  const $wrapper = document.createElement(`span`);
+  $wrapper.classList.add(`price__button__wrapper`);
+  $priceButton.appendChild($wrapper);
+
+  const $wrapperName = document.createElement(`span`);
+  $wrapperName.classList.add(`price__button__name`);
+  $wrapper.appendChild($wrapperName, $wrapperAmount, $wrapperPlus);
+
+  const $wrapperAmount = document.createElement(`span`);
+  $wrapperAmount.classList.add(`price__button__amount`);
+  $wrapper.appendChild($wrapperAmount);
+
+  const $wrapperPlus = document.createElement(`span`);
+  $wrapperPlus.classList.add(`price__button__plus`);
+  $wrapper.appendChild($wrapperPlus);
+
+  //HTML structuur invullen
+  const euro = `\u20ac`;
+
+  $wrapperName.textContent = coffeeName;
+  $wrapperAmount.textContent = `${euro} ${coffeePrice}`;
+  $wrapperPlus.textContent = `+`;
+};
+
+//Alle koffies in een array binnenhalen: console.log(data.coffees);
+//Van de vier eerste koffies de HTML opbouwen
+
+const init = () => {
+  const $pricelist = document.querySelector(`.prices__list`);
+
+  data.coffees.forEach(coffee => {
+    if (coffee.id < 5) {
+      const coffeeName = coffee.name;
+      const coffeePrice = coffee.prices.medium;
+      makeHTML($pricelist, coffeeName, coffeePrice);
+    }
+  });
+};
+
+init();
